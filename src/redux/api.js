7e834f5ee5ac709278c44d1c8bfbab788beae8f4
Riz <mahmoud.eleresousy@gmail.com>
2023-11-11 21:18:28 +0200

    Remove side effects -console-statement- of API.js file

--- src/redux/api.js
+++ src/redux/api.js
@@ -3,7 +3,6 @@ import { createAsyncThunk } from '@reduxjs/toolkit';
 const fetchData = createAsyncThunk('GREETING/FETCHRANDOM', async () => {
   const response = await fetch('http://127.0.0.1:3000');
   const ApiData = await response.json();
-  console.log(ApiData.greeting);
   return ApiData.greeting;
 });
 
